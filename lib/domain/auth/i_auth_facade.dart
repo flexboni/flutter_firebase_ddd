9d5a49c31e79d4b5cade8995f042559806d192b5
flexboni <kgb8594@gmail.com>
2021-03-26 11:46:26 +0900

    계정 권한을 위한 interface 설정 및 계정 오류 handling 코드 적용

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_firebase_ddd/domain/auth/auth_failure.dart';
+import 'package:flutter_firebase_ddd/domain/auth/value_objects.dart';
+
+/// i : interface, facade : design pattern
+/// The same level as 'repositories' int the layer diagram.
+abstract class IAuthFacade {
+  // Why we use 'Either & Unit', Treat null!!
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<void> signInWithGoogle();
+}
